dab44fa8543d6e0437e730a58ac1096de900eadd
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2025-09-26 22:46:34 -0300

    Add task creation functionality and improve UI
    Introduced ability to add new tasks with title and description via AddTask component. Updated App to manage task addition, and improved layout and styling for AddTask and Tasks components.

--- src/App.jsx
+++ src/App.jsx
@@ -41,13 +41,23 @@ function App() {
     setTasks(newTasks)
   }
 
+  function addTask(title, description){
+    const newTask = {
+      id: tasks.length + 1,
+      title,
+      description,
+      IsCompleted: false
+    }
+    setTasks([...tasks, newTask])
+  }
+
   return( 
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       
-      <div className="w-[500px]">
+      <div className="w-[500px] space-y-4">
         <h1 className="text-3xl text-slate-100 font-bold text-center">Gerenciandor de tarefas</h1>
-        <AddTask />
-        <Tasks tasks={tasks} onCheckTask={onCheckTask} deleteTask={deleteTask}/>
+        <AddTask addTask={addTask} />
+        <Tasks tasks={tasks} onCheckTask={onCheckTask} deleteTask={deleteTask} />
       </div>
     </div>
   
