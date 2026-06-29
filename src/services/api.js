a8fe2656cf9728e44ff0fd0a176d1ed631a9e60e
Vitor L Vilarindo <97807732+vitorvilarindo@users.noreply.github.com>
2026-06-29 11:19:21 -0300

    Remove stylesheet link and stray comment
    Remove the <link> to /src/style.css from index.html and delete a stray Portuguese comment in src/services/api.js. Cleans up an unused stylesheet reference and removes unnecessary inline comment from the API service file.

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { openPermissionModal } from "../context/withoutPermissionModalHandler.js";
-// função que abre modal (vamos criar abaixo)
 
 const api = axios.create({
     baseURL: import.meta.env.VITE_DATABASE_URL_ROOT,
